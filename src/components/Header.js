b6525afd228522b54f94ffd918712b81f9c0d0e2
Utkarsh Mishra <aditya@Adityas-MacBook-Air.local>
2024-09-12 03:14:27 +0530

    TMDB API data Fetch && built MainContainer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LOGO } from "../utils/constants";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
 
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-        navigate("/");
-      })
+      .then(() => {})
       .catch((error) => {
         navigate("/error");
       });
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <img className="w-10 h-10" src={user.photoURL} alt="usericon" />
+          <img className="w-10 h-10" src={user?.photoURL} alt="usericon" />
           <button className="font-bold text-white p-2" onClick={handleSignOut}>
             (Sign out)
           </button>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,14 @@ export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const USER_AVATAR =
-  "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+  "https://occ-0-4409-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABX4L9w79aT7nijZVgdNGkMraiqSPBtchAJv6ufbTi63POCqjyeH33eWZhDE7GITmH28pVz8rDrrSiJTYJIukt6h34Nyoqnw.png?r=eda";
 
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjMwZjM3ZjM1NGFhNzVkNWY2MTdjZWMxZWM5MzRhNSIsIm5iZiI6MTcyNjA1NTMyMC42Mzg1OTQsInN1YiI6IjY2ZTE4MjhmYzNmZjhmOTcwNzA4MTgxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NUhajlcFIosCg8JKqlzmZi4G_wA7RInmtjqzqdbiYk0",
   },
 };
 
